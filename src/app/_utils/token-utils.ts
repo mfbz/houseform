1607dfde5012f247a77b1ba8e5d2a1164861f568
mfbz <michael.fabozzi@icloud.com>
2023-11-24 22:25:27 +0100

    Minor fix

--- src/app/_utils/token-utils.ts
+++ src/app/_utils/token-utils.ts
@@ -1,9 +1,11 @@
 export class TokenUtils {
 	public static toNumber(value: bigint, decimals: number) {
+		if (value === null || value === undefined) return value;
 		return Number(BigInt(value) / BigInt(10 ** decimals));
 	}
 
 	public static toBigInt(value: number, decimals: number) {
+		if (value === null || value === undefined) return value;
 		return BigInt(value) * BigInt(10 ** decimals);
 	}
 }
